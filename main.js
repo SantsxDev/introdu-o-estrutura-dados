cdbf0d78881fccbd46b4ea6264155f6807e6914e
Ph.git <Phuguinho2018@gmail.com>
2025-04-10 00:22:33 +0000

    exemplo array

---
+++ main.js
@@ -0,0 +1,11 @@
+let alisson_nota_av1 = 8;
+ let alisson_nota_av2 = 9.6;
+ let alisson_media = (allisson_nota_av1)  +  alisson_nota_av2 / 2
+
+ console.log("Média Allisson: " + allisson_media);
+
+ console.log(exemplo);(com); (arrays) 
+ let notas_allisson=  [8,  9.6,   ];
+ console.log(notas_allisson);
+
+ console.log(AV1)
